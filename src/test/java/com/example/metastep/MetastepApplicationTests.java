4e95347574712dacc51068346d2c17acb9f63278
Abdel <hamdo.abdo.22@gmail.com>
2022-09-20 14:24:02 +0100

    create the project (first commit)

---
+++ src/test/java/com/example/metastep/MetastepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metastep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetastepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
